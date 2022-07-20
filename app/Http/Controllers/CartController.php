259d37db829fe344fdda5044d01b308de9c9273b
Administrator <niergx8.29@gmail.com>
2022-07-20 03:41:07 +0000

    admin_system

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -98,17 +98,44 @@ class CartController extends Controller
      */
     public function destroy(Request $request)
     {
-        $user_shoppingcarts = DB::table('shoppingcart')->where('instance', Auth::user()->id)->get();
+        $user_shoppingcarts = DB::table('shoppingcart')->get();
+        $number = DB::table('shoppingcart')->where('instance', Auth::user()->id)->count();
  
-         $count = $user_shoppingcarts->count();
+        $count = $user_shoppingcarts->count();
  
-         $count += 1;
-         Cart::instance(Auth::user()->id)->store($count);
+        $count += 1;
+        $number += 1;
+        $cart = Cart::instance(Auth::user()->id)->content();
  
-         DB::table('shoppingcart')->where('instance', Auth::user()->id)->where('number', null)->update(['number' => $count, 'buy_flag' => true]);
+        $price_total = 0;
+        $qty_total = 0;
  
-         Cart::instance(Auth::user()->id)->destroy();
+        foreach ($cart as $c) {
+            if ($c->options->carriage) {
+                $price_total += ($c->qty * ($c->price + 800));
+            } else {
+                $price_total += $c->qty * $c->price;
+            }
+            $qty_total += $c->qty;
+        }
  
-         return redirect()->route('carts.index');
+        Cart::instance(Auth::user()->id)->store($count);
+ 
+        DB::table('shoppingcart')->where('instance', Auth::user()->id)
+                                 ->where('number', null)
+                                 ->update(
+                                     [
+                                         'code' => substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 10),
+                                         'number' => $number, 
+                                         'price_total' => $price_total,
+                                         'qty' => $qty_total,
+                                         'buy_flag' => true, 
+                                         'updated_at' => date("Y/m/d H:i:s")
+                                     ]
+                                 );
+ 
+        Cart::instance(Auth::user()->id)->destroy();
+ 
+        return redirect()->route('carts.index');
     }
 }
